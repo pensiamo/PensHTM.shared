2f163b453e7d80e924ad49c7fd1f4bb27b5de413
Michelle Bergman <michelle.bergman@pensiamoinc.com>
2020-01-23 16:00:11 -0500

    Testing vscode push

--- Themes.js
+++ Themes.js
@@ -20,7 +20,8 @@ const colorWheel = {
     black: '#000',
     greyTan: 'rgba(152, 151, 139, 0.5)',
     testColor: 'pink',
-    anotherColor: 'blue'
+    anotherColor: 'blue',
+    yetanothercolor: 'green'
 }
 
 export default colorWheel;
