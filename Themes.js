1f770a56f85b759d2abe26a08c74bfb64e347830
Michelle Bergman <michelle.bergman@pensiamoinc.com>
2020-01-23 13:48:03 -0500

    Added Themes file and deleted test file

---
+++ Themes.js
@@ -0,0 +1,24 @@
+const colorWheel = {
+    blue: '#007C92',
+    orange: '#DC5034',
+    green: '#739600',
+    maroon: '#AA1948',
+    yellow: '#EAAB00',
+    gray: '#747678',
+    transGreen: 'rgb(115, 150, 0, 0.25)',
+    transYellow: 'rgb(255, 207, 0, 0.25)',
+    mainGrayTan: '#98978b',
+
+    red: '#c10000',
+    bodyTan: 'rgba(147, 128, 108, .15)',
+    lightTan: 'rgb(247, 246, 244)',
+    mediumTan: 'rgb(221, 221, 221)',
+    darkTan: 'rgb(130, 130, 129)',
+    tan: 'rgb(181, 181, 177)',
+    darkGray: '#495057',
+    white: '#fff',
+    black: '#000',
+    greyTan: 'rgba(152, 151, 139, 0.5)'
+}
+
+export default colorWheel;
