da2f9592c5f7fb5dbb4768dd653411c4610e133d
Michelle Bergman <mymichellebergman@gmail.com>
2020-01-24 12:02:36 -0500

    Added fontAwesome icons function to be used in App.js. Also moved Themes.js to a folder.

---
+++ fontAwesome/icons.js
@@ -0,0 +1,29 @@
+import {
+    faTools, faSearch, faEraser, faCheckCircle, faTimesCircle, faEdit, faCheck, faMinusCircle, faClipboardList,
+    faSitemap, faChevronUp, faChevronDown, faUserTag, faUserLock, faUsersCog, faUserFriends, faUserTimes, faUserMinus,
+    faUserPlus, faDatabase, faUserEdit, faSave, faUndo, faFileImport, faChevronRight, faExclamationCircle,
+    faExclamationTriangle, faSync, faFolderPlus, faCogs, faTag, faIndustry, faBriefcase, faPlusCircle,
+    faSquare, faCheckSquare, faInfoCircle, faTimes, faDownload, faUpload, faClone, faEllipsisH, faStepForward,
+    faBarcode, faHashtag, faBan, faClock, faCalendarCheck, faAlignJustify, faHandPaper, faCopy, faCommentDots,
+    faPencilAlt, faFolder, faTh, faBookOpen, faLink, faList, faThumbsUp, faCog,
+} from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+const addFontAwesomeIconsToLibrary = () => {
+    // library.add(comma delimited list of imports from
+    // '@fortawesome/free-solid-svg-icons'); then used in component...  import {
+    // FontAwesomeIcon } from '@fortawesome/react-fontawesome'  <FontAwesomeIcon
+    // icon='check-square' size='lg' />
+    library.add(faTools, faSearch, faEraser, faCheckCircle, faTimesCircle, faEdit, faCheck,
+        faMinusCircle, faClipboardList, faSitemap, faChevronUp, faChevronDown, faUserTag,
+        faUserLock, faUsersCog, faUserFriends, faUserTimes, faUserMinus, faUserPlus, faDatabase,
+        faUserEdit, faSave, faUndo, faFileImport, faChevronRight, faExclamationCircle, faExclamationTriangle, faSync,
+        faFolderPlus, faCogs, faTag, faIndustry, faBriefcase, faPlusCircle, faSquare, faCheckSquare, faInfoCircle,
+        faTimes, faCheck, faDownload, faUpload, faClone, faEllipsisH, faStepForward, faBarcode, faHashtag, faBan,
+        faClock, faCalendarCheck, faAlignJustify, faHandPaper, faCopy, faCommentDots, faPencilAlt, faFolder, faTh,
+        faBookOpen, faLink, faList, faThumbsUp, faCog);
+};
+
+export {
+    addFontAwesomeIconsToLibrary
+}
